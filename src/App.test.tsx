80d0fd5a2a59814d72bc81a391d7968cf6838676
Dan Margalit <danm1980@gmail.com>
2025-09-08 13:49:52 +0300

    mobile bugfixes, optamization

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Ella Estate site', () => {
-  render(<App />);
-  const siteNameElement = screen.getByText(/Ella Estate/i);
-  expect(siteNameElement).toBeInTheDocument();
-});
